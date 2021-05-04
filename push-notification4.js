f40d4d8c80e3ae22ebd4a9c1a51747078e135f7e
Utkarsh Kashyap <60151123+utkarsh-777@users.noreply.github.com>
2021-05-04 13:09:42 +0530

    Create push-notification4.js

---
+++ push-notification4.js
@@ -0,0 +1,35 @@
+const public_vapid_key = "BJfiHyy_qFOmXwlk4ntA7BYLWFSWYDC-5Yv6lD6Di7TwmNwpIcJbtWlyIRwIHIRDgtRtXdh2jtrMEgxaCzVUP9c";
+
+//check for service worker
+
+if('serviceWorker' in navigator){
+    send().catch(err => console.log(err));
+}
+
+async function send() {
+    const register = await navigator.serviceWorker.register('./push-notification1.js',{
+        scope: '/'
+    });
+
+const subscription = await register.pushManager.subscribe({
+    userVisibleOnly:true,
+    applicationServerKey:urlBase64ToUint8Array(public_vapid_key)
+});
+  
+console.log('Push Registered...')
+
+
+function urlBase64ToUint8Array(base64String) {
+    var padding = '='.repeat((4 - base64String.length % 4) % 4);
+    var base64 = (base64String + padding)
+        .replace(/\-/g, '+')
+        .replace(/_/g, '/');
+
+    var rawData = window.atob(base64);
+    var outputArray = new Uint8Array(rawData.length);
+
+    for (var i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+}
